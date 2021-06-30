f2f4712daa5138160f4831fd574c15747b4a2084
Joaquin <andres16081990@gmail.com>
2021-06-30 00:09:02 -0500

    reto 6

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import express from 'express'
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true}));
+
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="/" method="POST">
+            <label for="get_name"></label>
+            <input type="text" name="name" id="name">
+            <label for="send"></label>
+            <button>Enviar</button>
+        </form>
+        
+    </body>
+    </html>`)
+})
+
+app.post('/',(req,res)=>{
+    const {name}= req.body
+    res.status(200).send(`<h1>Hola ${name}!</h1>`)
+})
+
+app.listen(port,()=>console.log(`server rinung in port ${port}`))
+
